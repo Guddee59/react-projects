cd4d0d8b2789ff4cb31cb882311286d5d7140a37
Guddee59 <guddeenoudiyal59@gmail.com>
2026-04-05 17:36:34 +0530

    build fixes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -42,7 +42,7 @@ header{
      );
      sections.forEach((s) => obs.observe(s));
      return () => obs.disconnect();
-   }, [ NAV,setActive]);
+   }, [setActive]);
     return (
     <MainContaier>
      <header className="header">
